66346b3ee6425e55bdc1761271c314f61db0af99
korzck <konstantin.koretskiy@gmail.com>
2022-11-05 18:27:44 +0300

    added testing

---
+++ lab_python_test/main.py
@@ -0,0 +1,36 @@
+import sys
+#changed cwd to testible dir
+sys.path.append("../lab_python_oop/lab_python_oop")
+from Circle import Circle
+from Rectangle import Rectangle
+from Square import Square
+
+import unittest
+
+class MyTesting(unittest.TestCase):
+    def setUp(self):
+        self.a = Rectangle(6, 6, 'blue', 'pryamougolnik')
+        self.b = Circle(6, 'green', 'krug')
+        self.c = Square(3, 'red', 'kvadrat')
+    
+    def test_area(self):
+        import math
+        self.assertEqual(self.a.area(), 36)
+        self.assertEqual(self.b.area(), math.pi*6**2)
+        self.assertEqual(self.c.area(), 9)
+    
+    def test_color(self):
+        self.assertEqual(self.a.color.value, 'blue')
+        self.assertEqual(self.b.color.value, 'green')
+        self.assertEqual(self.c.color.value, 'red')
+    
+    def test_get_name(self):
+        self.assertEqual(self.a.get_name(), 'pryamougolnik')
+        self.assertEqual(self.b.get_name(), 'krug')
+        self.assertEqual(self.c.get_name(), 'kvadrat')
+        
+
+if __name__ == '__main__':
+    unittest.main()
+
+
