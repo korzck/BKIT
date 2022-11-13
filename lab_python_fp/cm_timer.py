9b193e5af5efa4f672ccd69473da78f5bba1f1a6
korzck <konstantin.koretskiy@gmail.com>
2022-11-13 13:36:27 +0300

    deleted useless lines

--- lab_python_fp/cm_timer.py
+++ lab_python_fp/cm_timer.py
@@ -12,9 +12,3 @@ def cm_timer_2():
     start_time = time.time()
     yield 
     print(time.time()-start_time)
-
-with cm_timer_1() as c:
-    time.sleep(2.6)
-
-with cm_timer_2() as c:
-    time.sleep(2.6)    
